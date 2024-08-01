5ae55c1a6f69579c17d557347dd7961d699dee4b
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-08-01 13:10:44 +0530

    profile image cropper done

--- src/components/dashboard/Profile.svelte
+++ src/components/dashboard/Profile.svelte
@@ -18,6 +18,7 @@ let uncropped = null;
 function onfileselected(e){
     let backdrop = document.getElementById("backdrop");
     let cropper = document.getElementById("cropper");
+    let cropper_parent = document.getElementById("cropper_parent");
     let image = e.target.files[0];
     let reader = new FileReader();
     reader.readAsDataURL(image);
@@ -26,23 +27,42 @@ function onfileselected(e){
     backdrop.classList.remove("hidden");
     cropper.classList.remove("hidden");
     cropper.classList.add("flex");
+    cropper_parent.classList.remove("hidden");
+    cropper_parent.classList.add("flex");
 };
 
 
 
 }
 let pixelCrop = null;
+let crop = { x: 0, y: 0 }
+let zoom = 1
 async function cropImage(){
         console.log("cropping image");
 		avatar = await getCroppedImg(uncropped, pixelCrop);
         profile_update = true;
         let backdrop = document.getElementById("backdrop");
         let cropper = document.getElementById("cropper");
+        let cropper_parent = document.getElementById("cropper_parent");
         backdrop.classList.add("hidden");
         cropper.classList.add("hidden");
         cropper.classList.remove("flex");
+        cropper_parent.classList.add("hidden");
+        zoom = 1;
+        crop = { x: 0, y: 0 }
 }
 
+function cancelCrop() {
+    let backdrop = document.getElementById("backdrop");
+    let cropper = document.getElementById("cropper");
+    let cropper_parent = document.getElementById("cropper_parent");
+    backdrop.classList.add("hidden");
+    cropper.classList.add("hidden");
+    cropper.classList.remove("flex");
+    cropper_parent.classList.add("hidden");
+    zoom = 1;
+    crop = { x: 0, y: 0 }
+}
 
 function handleupdateprofile(){
     let name = document.getElementById("name").value;
@@ -71,23 +91,27 @@ function handleupdateprofile(){
         window.location.href = '/dashboard'
     })
 }
-let crop = { x: 0, y: 0 }
-let zoom = 1
+
 </script>
 
 
-    <div class="w-96 h-60 z-20 absolute hidden top-[20%] md:left-[40%] left-[10%]">
-        <div style="height: 500px" class="z-40 hidden items-center justify-center bg-white " id="cropper">
+    <div class="w-96 h-60 z-20 hidden flex-col items-center bg-white absolute top-[20%] md:left-[40%] left-[10%]" id="cropper_parent">
+        <div class="z-40  items-center justify-center bg-white w-full h-full" id="cropper">
             <Cropper
             image={uncropped}
+            aspect={1}
             bind:crop
             bind:zoom
+            cropShape="round"
             on:cropcomplete={e => pixelCrop = e.detail.pixels}
             />
             
-            <button type="button" on:click={cropImage}>Crop!</button>
         </div>
+        <div class="flex justify-evenly w-full">
 
+            <button class="bg-blue-500 z-40 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs md:text-sm" on:click={cropImage}>Crop</button>
+            <button class="bg-red-600 z-40 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-xs md:text-sm" on:click={cancelCrop}>Cancel</button>
+        </div>
     </div>
 
 <div class="fixed  pin items-center hidden bg-black w-screen h-screen opacity-60 z-10 " id="backdrop"></div>
@@ -110,43 +134,43 @@ let zoom = 1
    </div>
 </div>
 
-<div class="flex flex-col justify-center pt-8">
+<div class="flex flex-col justify-center pt-3">
 
     
     <form class="px-4 md:px-40 flex flex-col"  on:submit|preventDefault={handleupdateprofile}> 
-        <div class="flex justify-between mt-10">
-            <label for="name" class="text-black text-lg md:text-3xl lg:text-4xl   ">Name:</label>
+        <div class="flex justify-between mt-6">
+            <label for="name" class="text-black text-lg md:text-3xl lg:text-4xl  p-1 ">Name:</label>
             <input type="text" name="name" id="name" placeholder="Full Name" class="text-lg md:text-2xl lg:text-3xl text-black hover:text-black p-1  border border-black" value={details.name}>
         </div>
 
-        <div class="flex justify-between  mt-10">
-            <label for="email" class="text-black text-lg md:text-3xl lg:text-4xl ">Email:</label>
+        <div class="flex justify-between  mt-6">
+            <label for="email" class="text-black text-lg md:text-3xl lg:text-4xl p-1">Email:</label>
             <input type="email" name="email" id="email" placeholder="Email" class="text-lg md:text-2xl lg:text-3xl text-black hover:text-black p-1 border border-black" value={details.email}>
         </div>
 
-         <div class="flex justify-between  mt-10">
-            <label for="gender" class="text-black text-lg md:text-3xl lg:text-4xl">Gender:</label>
+         <div class="flex justify-between  mt-6">
+            <label for="gender" class="text-black text-lg md:text-3xl lg:text-4xl p-1">Gender:</label>
             <input type="text" name="gender" id="gender" placeholder="Gender" class="text-lg md:text-2xl lg:text-3xl text-black hover:text-black p-1  border border-black" value={details.gender?? ""}>
         </div>
 
-        <div class="flex justify-between  mt-10">
-            <label for="pronouns" class="text-black text-lg md:text-3xl lg:text-4xl">Pronouns:</label>
+        <div class="flex justify-between  mt-6">
+            <label for="pronouns" class="text-black text-lg md:text-3xl lg:text-4xl p-1">Pronouns:</label>
             <input type="text" name="pronouns" id="pronouns" placeholder="Pronouns" class="text-lg md:text-2xl lg:text-3xl text-black hover:text-black p-1  border border-black"  value={details.pronouns?? ""}>
         </div>
 
-        <div class="flex justify-between  mt-10">
-            <label for="batch" class="text-black text-lg md:text-3xl lg:text-4xl  ">Batch:</label>
+        <div class="flex justify-between  mt-6">
+            <label for="batch" class="text-black text-lg md:text-3xl lg:text-4xl p-1 ">Batch:</label>
             <input type="text" name="batch" id="" placeholder="Batch" value={details.batch} class="text-lg md:text-2xl lg:text-3xl text-black hover:text-black p-1  border border-black" disabled >
         </div>
 
-        <div class="flex justify-between  mt-10">
-            <label for="Role" class="text-black text-lg md:text-3xl lg:text-4xl ">Role:</label>
+        <div class="flex justify-between  mt-6">
+            <label for="Role" class="text-black text-lg md:text-3xl lg:text-4xl p-1 ">Role:</label>
             <input type="text" name="Role" id="" placeholder="Role" value={details.role} class="text-lg md:text-2xl lg:text-3xl text-black hover:text-black p-1 border border-black" disabled>
         </div>
 
-        <div class="flex justify-center  mt-20">
+        <div class="flex justify-center  mt-12">
         <button type="button" class="text-white bg-gradient-to-r text-lg md:text-3xl lg:text-4xl from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2">
-            Submit</button>
+            Update</button>
 
         </div> 
     </form>
